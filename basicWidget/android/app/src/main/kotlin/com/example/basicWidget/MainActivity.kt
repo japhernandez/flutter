b5e837708dcb8aee1a8537581dd404075ef423d5
John Piedrahita <citymanager02@gmail.com>
2020-06-13 22:27:59 -0500

    Archivos faltantes

---
+++ basicWidget/android/app/src/main/kotlin/com/example/basicWidget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.basicWidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ basicWidget/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:basicWidget/ui/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Simple Layout',
+    home: new Home(),
+  ));
+}
