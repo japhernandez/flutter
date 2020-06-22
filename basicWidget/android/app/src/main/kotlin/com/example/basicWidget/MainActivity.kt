be87d12a6caa44d1f70e21750a23fb73fbb06838
John Piedrahita <citymanager02@gmail.com>
2020-06-22 18:00:04 -0500

    Remove files

--- basicWidget/android/app/src/main/kotlin/com/example/basicWidget/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.basicWidget
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- basicWidget/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:basicWidget/ui/home.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(new MaterialApp(
-    title: 'Simple Layout',
-    home: new Home(),
-  ));
-}
