a0e0ffbb2fc6b4bff20c9f5292e045fb19d3150b
John Piedrahita <citymanager02@gmail.com>
2020-08-26 20:26:21 -0500

    Animaciones

--- animations/lib/main.dart
+++ animations/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:animations/src/pages/header.dart';
+import 'package:animations/src/pages/animations.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Diseños App',
       debugShowCheckedModeBanner: false,
-      home: Header(),
+      home: Animations(),
     );
   }
 }
